a3e9a4aedbf926a73c6bde470cf69b02b2a2d1a2
yuki622 <nakayuki622@gmail.com>
2021-06-20 17:43:16 +0900

    フェイカー

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Room::class, function (Faker $faker) {
     return [
-        'Studio_id' => $faker -> numberBetween(1, 10),
+        'studio_id' => $faker -> numberBetween(1, 10),
         'type' => $faker->title,
         'description' => $faker->text(100),
         'price' => $faker->numberBetween(1000.15000),
